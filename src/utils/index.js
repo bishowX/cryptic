d6ee47d33ea8e84face9cc3e7a28be96b740f5e2
bishowX <94373953+bishowX@users.noreply.github.com>
2022-03-22 10:59:26 +0545

    delete unused utils

--- src/utils/index.js
+++
@@ -1,5 +0,0 @@
-export const handleFormValueChange = (event, formValues) => {
-  const newFormValues = { ...formValues };
-  newFormValues[event.target.name] = event.target.value;
-  setFormValues(newFormValues);
-};
