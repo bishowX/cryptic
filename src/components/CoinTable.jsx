5ddecd775b0eb003bcfaac4af630c7869ff4c445
bishowX <94373953+bishowX@users.noreply.github.com>
2022-03-24 20:16:39 +0545

    optimized render of that sucker(coins fetched from api)

--- src/components/CoinTable.jsx
+++ src/components/CoinTable.jsx
@@ -1,6 +1,38 @@
+import { useState } from "react";
 import Link from "next/link";
+import FlatList from "flatlist-react";
 
 const CoinTable = ({ coins }) => {
+  const [limit, setLimit] = useState(20);
+
+  const renderCoin = (coin, idx) => {
+    return (
+      <tr className="coin_row" key={coin.asset_id}>
+        <td>
+          <Link href={`/coin/${coin.asset_id}`}>
+            <a>{coin.asset_id}</a>
+          </Link>
+        </td>
+        <td>
+          <Link href={`/coin/${coin.asset_id}`}>
+            <a>{coin.name}</a>
+          </Link>
+        </td>
+        <td>
+          <Link href={`/coin/${coin.asset_id}`}>
+            <a>{coin.price_usd}</a>
+          </Link>
+        </td>
+      </tr>
+    );
+  };
+
+  const loadMore = () => {
+    let newLimit = limit + 20;
+    if (newLimit > coins.length) newLimit = coins.length;
+    setLimit(newLimit);
+  };
+
   return (
     <table>
       <thead>
@@ -11,28 +43,9 @@ const CoinTable = ({ coins }) => {
         </tr>
       </thead>
       <tbody>
-        {coins.map((coin, index) =>
-          coin.type_is_crypto || index > 10 ? (
-            <tr className="coin_row" key={coin.asset_id}>
-              <td>
-                <Link href={`/coin/${coin.asset_id}`}>
-                  <a>{coin.asset_id}</a>
-                </Link>
-              </td>
-              <td>
-                <Link href={`/coin/${coin.asset_id}`}>
-                  <a>{coin.name}</a>
-                </Link>
-              </td>
-              <td>
-                <Link href={`/coin/${coin.asset_id}`}>
-                  <a>{coin.price_usd}</a>
-                </Link>
-              </td>
-            </tr>
-          ) : null
-        )}
+        <FlatList limit={limit} paginate list={coins} renderItem={renderCoin} renderOnScroll />
       </tbody>
+      <button onClick={loadMore}>Load More</button>
     </table>
   );
 };
--- src/pages/market.jsx
+++ src/pages/market.jsx
@@ -1,6 +1,6 @@
+import { useEffect } from "react";
 import CoinTable from "components/CoinTable";
 import { useCoin } from "context/CoinContext";
-import { useEffect } from "react";
 
 const Market = () => {
   const {
